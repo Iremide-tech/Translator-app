07cef552de673c2ded3b7dc62f50594b079d62ef
Iremide-tech <iresamuel226@gmail.com>
2025-01-03 15:32:04 +0100

    Add files via upload

---
+++ country.js
@@ -0,0 +1,15 @@
+const countries = {
+    "Id-ID": "Indonesian",
+    "ar-SA": "Arabic",
+    "de-DE": "German",
+    "it-IT": "Italian",
+    "es-ES": "Spanish",
+    "ja-JP": "Japanese",
+    "en-GB": "English",
+    "fr-FR": "French",
+    "ha-HA": "Hausa",
+    "hi-HI": "Hindi",
+    "ge-GE": "German",
+    "gr-GR": "Greek",
+    "gu-GU": "Gujarati",
+  }
